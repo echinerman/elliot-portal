188c8fd52918dccdaa7848ba0fb4ae73bc44402b
echinerman <e.chinerman@gmail.com>
2026-02-02 22:12:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,20 @@
-// --- CONFIGURATION ---
 const CONFIG = {
-    // Your Admin Email - This acts as the "Master Key"
-    ADMIN_EMAIL: "e.chinerman@gmail.com", 
+    // Admin Master Key
+    ADMIN_EMAIL: "e.chinerman@gmail.com",
     
-    // The Invite Code required for new registrations
-    INVITE_CODE: "FAMILY8K", 
-    
-    // Contact Links
+    // Contact Link
     FB_LINK: "https://m.me/rds.ca",
-    
-    // Firebase Configuration (Paste your keys here)
+
+    // Firebase Configuration
     FIREBASE: {
         apiKey: "AIzaSyAH5CRCJoktmY6baXshDnOVgylHldcPa3E",
         authDomain: "elliotvip.firebaseapp.com",
         projectId: "elliotvip",
         storageBucket: "elliotvip.firebasestorage.app",
         messagingSenderId: "223494244969",
-        appId: "1:223494244969:web:edcf227f756ac76ba170f8"
+        appId: "1:223494244969:web:edcf227f756ac76ba170f8",
+        measurementId: "G-7591L9DYT6"
     }
 };
 
-// Export for module use
 export default CONFIG;
